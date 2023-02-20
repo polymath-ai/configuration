3e5ecfeebf11a492c5ae20ab83f29072d75b9fe6
Dimitri Glazkov <dimitri@glazkov.com>
2023-02-20 15:47:51 -0800

    First cut at config validation.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('Configuration System goes here.');
+import Ajv from 'ajv'
+import { JTDDataType } from 'ajv/dist/core';
+
+export type ConfigType<S> = JTDDataType<S>;
+
+export function Config<T>(schema: any, location?: string) {
+  const validator = new Ajv({ useDefaults: true }).compile<T>(schema);
+  return {
+    location: location,
+    validate: validator,
+    create<T>(args: any): T {
+      if (!validator(args)) {
+        throw new Error(`Invalid config: ${validator.errors?.map(e => e.message).join('\n')}`);
+      }
+      return args as unknown as T;
+    }
+  }
+}
--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,70 @@
 import test from 'ava';
+import { Config, ConfigType } from './index.js';
 
-const fn = () => 'foo';
+const simple_schema = {
+  type: "object",
+  properties: {
+    bar: { type: "string", default: 'simple' },
+    baz: { type: "integer", default: 42 },
+    items: { type: "array", items: { type: "integer" }, default: [] },
+    bag: {
+      type: "object",
+      properties: {
+        values: { type: "integer" },
+      },
+      default: {},
+    },
+  },
+}; 
 
-test('fn() returns foo', t => {
-	t.is(fn(), 'foo');
+type SimpleConfigType = ConfigType<typeof simple_schema>;
+const SimpleConfig = Config(simple_schema, 'simple');
+
+
+test('simple config', t => {
+  let simple_args: any = {
+    'bar': 'bar',
+    'baz': 0,
+    'items': [1, 2, 3],
+    'bag': { 'a': 1 }
+  }
+
+  t.true(SimpleConfig.validate(simple_args));
 });
+
+test('simple config defaults', t => {
+  const simple_config: SimpleConfigType = SimpleConfig.create({});
+  t.is(simple_config.bar, 'simple');
+});
+
+test('config has location', t => {
+  t.is(SimpleConfig.location, 'simple');
+});
+
+const required_field_schema = {
+  type: "object",
+  properties: {
+    bar: { type: "string" },
+    baz: { type: "integer", default: 42 },
+  },
+  required: ['bar']
+}; 
+
+type RequiredFieldConfigType = ConfigType<typeof required_field_schema>;
+const RequiredFieldConfig = Config(required_field_schema);
+
+test('required fields supplied', t => {
+  t.true(RequiredFieldConfig.validate({ bar: 'bar' }));
+});
+
+test('complain when required field is missing', t => {
+  t.false(RequiredFieldConfig.validate({}));
+});
+
+
+// class RequiredFieldConfig extends Config {
+//   bar: string;
+//   baz: number = 42;
+
+//   constructor(args: any) { super(args) }
+// }

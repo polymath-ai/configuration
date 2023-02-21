053dea64c794b890be26709a81c0b0edd79df626
Dimitri Glazkov <dimitri@glazkov.com>
2023-02-20 20:09:46 -0800

    Even more types.

--- src/json.ts
+++ src/json.ts
@@ -1,6 +1,7 @@
 /* Adapter for loading and saving configuration as JSON files */
 
 import { promises as fs } from 'fs';
+import { ConfigHandler } from '.';
 
 export class JSONStore {
   root: string;
@@ -9,7 +10,7 @@ export class JSONStore {
     this.root = root;
   }
 
-  async load<T>(config: any): Promise<T> {
+  async load<T>(config: ConfigHandler<T>): Promise<T> {
     const filename = `${this.root}/config.${config.location}.json`
     const data: any = await fs.readFile(filename, 'utf8');
     return config.create(JSON.parse(data)) as T;
